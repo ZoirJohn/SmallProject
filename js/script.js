7447684555b3ec799aa93208fa085d47323482fe
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2022-11-03 16:13:51 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+// console.clear();
+// Magic header
+// const  = document.querySelector('');
+// window.addEventListener('scroll', fixing);
+// function fixing() {
+//    if (scrollY <= .getBoundingClientRect().height * 2) {
+//       .style.transform = `translate(0,-${scrollY}px)`;
+//       .classList.remove('p');
+//    } else {
+//       .classList.add('p');
+//    }
+// }
